c3ff8e5495e6b80b7a63584167051de60ca13e92
nhanvi2308 <nhan.vi@gleads.vn>
2021-10-02 19:15:08 +0700

    todo app

---
+++ todo/src/app/todoItem.component.ts
@@ -0,0 +1,15 @@
+
+
+export class TodoItem {
+
+
+  constructor(taskVal: string, completeVal: boolean = false) {
+      this.task = taskVal;
+      this.complete = completeVal;
+  }
+
+  task: string;
+  complete: boolean;
+
+
+}
